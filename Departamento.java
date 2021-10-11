06059b0900796ee61d1093056debe4fc00d82c99
claracdv <claracunhadavila@gmail.com>
2021-10-11 18:29:09 -0300

    adiciona construtor e atributos

---
+++ Departamento.java
@@ -0,0 +1,16 @@
+public class Departamento
+{
+	private int idDepartamento;
+	private int valorMaximo;
+	private String nomeDepartamento;
+	private int admId;
+	
+	public Departamento(int idDepartamento, int valorMaximo, String nomeDepartamento, int admId) {
+	    this.idDepartamento = idDepartamento;
+	    this.valorMaximo = valorMaximo;
+	    this.nomeDepartamento = nomeDepartamento;
+        this.admId = admId;
+	}
+	
+	
+}
