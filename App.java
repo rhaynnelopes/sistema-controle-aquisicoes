58cb69c8f53f13feda0dea1a453c0b24d92d5d7f
Raquel Severo <raquel.sev.celia@gmail.com>
2021-10-15 17:55:57 -0300

    Arrumando métodos Usuário
    Arrumamos um bug na classe App referente ao método adicionaFuncionario
    
    Co-Authored-By: Rhaynne Lopes <81125900+rhaynnelopes@users.noreply.github.com>

--- App.java
+++ App.java
@@ -5,12 +5,10 @@ public class App {
     public static void main(String[] args) {
 
 
-        //Usuario funcionario1 = new Usuario (01, 2, "Claudio Roberto", 3);
-        //Usuario funcionario2 = new Usuario (02, 1, "Laura ", 3);
+        Usuario funcionario1 = new Usuario (01, 2, "Claudio Roberto", 3);
+        Usuario funcionario2 = new Usuario (02, 1, "Laura ", 3);
 
-
-        // adicionaFuncionario.ListaUsuarios(funcionario1);
-        //adicionaFuncionario.ListaUsuarios(funcionario1);
+        
 
         int usuarioLogado;
         Scanner in = new Scanner(System.in);
@@ -20,11 +18,7 @@ public class App {
         usuarioLogado = in.nextInt();
 
 
-
-
     }
-
-
     
 }
 
