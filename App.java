b8fcd47439574f14dd66435302137694867219a7
William Schneider <william.lima.schneider@gmail.com>
2021-10-17 15:11:16 -0300

    simplificar leitura do APP

--- App.java
+++ App.java
@@ -1,31 +1,30 @@
-import java.util.Scanner;
 import java.util.ArrayList;
-
-    
-
-   
 
 public class App {
     public static void main(String[] args) {
 
-        ArrayList<Usuario> listaUsuarios= new ArrayList<Usuario>();
-        
-        listaUsuarios.add( new Usuario (01, 2, "Claudio Roberto", "CR", 3));
-        listaUsuarios.add( new Usuario (02, 2, "Laura Fonseca", "LF", 2));
-        listaUsuarios.add( new Usuario (03, 1, "Alice Silva", "AS", 1));
+        ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
 
+        criarEExibirListaUsuarios(listaUsuarios);
+
+    }
+
+    private static void criarEExibirListaUsuarios(ArrayList<Usuario> listaUsuarios) {
+        criarListaUsuarios(listaUsuarios);
+        exibirListaUsuarios(listaUsuarios);
+    }
+
+    private static void criarListaUsuarios(ArrayList<Usuario> listaUsuarios) {
+        listaUsuarios.add(new Usuario(01, 2, "Claudio Roberto", "CR", 3));
+        listaUsuarios.add(new Usuario(02, 2, "Laura Fonseca", "LF", 2));
+        listaUsuarios.add(new Usuario(03, 1, "Alice Silva", "AS", 1));
+
+    }
+
+    private static void exibirListaUsuarios(ArrayList<Usuario> listaUsuarios) {
         for (Usuario usuario : listaUsuarios) {
             System.out.println(usuario.toString());
         }
-
-
-        System.out.println("Selecione o seu usuário (insira o ID do usuário escolhido):");
-        //ListaUsuarios.imprimeLista();
-        //usuarioLogado = in.nextInt();
-
-
     }
-    
+
 }
-
-
