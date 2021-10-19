e2022f72b4fbb98257d80bb9c86479b3b2b73b66
William Schneider <william.lima.schneider@gmail.com>
2021-10-19 12:59:48 -0300

    mudar apresentacao da lista

--- App.java
+++ App.java
@@ -8,7 +8,6 @@ public class App {
 
         criarEExibirListaUsuarios(listaUsuarios);
         criarEExibirListaDepartamentos(listaDepartamentos);
-
     }
 
     private static void criarEExibirListaUsuarios(ArrayList<Usuario> listaUsuarios) {
@@ -31,24 +30,30 @@ public class App {
     }
 
     private static void criarListaDepartamentos(ArrayList<Departamento> listaDepartamentos) {
-        listaDepartamentos.add(new Departamento(1, 200, "Financeiro", 3));
-        listaDepartamentos.add(new Departamento(2, 100, "RH", 2));
-        listaDepartamentos.add(new Departamento(3, 100, "Engenharia", 1));
-        listaDepartamentos.add(new Departamento(4, 200, "Manutencao", 5));
-        listaDepartamentos.add(new Departamento(5, 100, "Administrativo", 4));
+        listaDepartamentos.add(new Departamento(1, 200, "Financeiro", 03));
+        listaDepartamentos.add(new Departamento(2, 100, "RH", 05));
+        listaDepartamentos.add(new Departamento(3, 100, "Engenharia", 03));
+        listaDepartamentos.add(new Departamento(4, 200, "Manutencao", 03));
+        listaDepartamentos.add(new Departamento(5, 100, "Administrativo", 05));
 
     }
 
     private static void exibirListaUsuarios(ArrayList<Usuario> listaUsuarios) {
+        System.out.println("\n--------------------\n");
         for (Usuario usuario : listaUsuarios) {
             System.out.println(usuario.toString());
+          
         }
+      
     }
 
     private static void exibirListaDepartamentos(ArrayList<Departamento> listaDepartamentos) {
+        System.out.println("\n--------------------\n");
         for (Departamento departamento : listaDepartamentos) {
             System.out.println(departamento.toString());
+          
         }
+     
     }
 
 }
