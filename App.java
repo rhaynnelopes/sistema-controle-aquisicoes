1360ae799f93590ecb91fac1fc275562c614e78a
claracdv <claracunhadavila@gmail.com>
2021-10-19 18:55:12 -0300

    Adiciona mais usuários ao app
    Co-Authored-By: Raquel Severo  <81184548+RaquelSev@users.noreply.github.com>

--- App.java
+++ App.java
@@ -22,12 +22,22 @@ public class App {
     }
 
     private static void criarListaUsuarios(ArrayList<Usuario> listaUsuarios) {
-        listaUsuarios.add(new Usuario(01, 2, "Claudio Roberto", "CR", 3));
-        listaUsuarios.add(new Usuario(02, 2, "Laura Fonseca", "LF", 2));
-        listaUsuarios.add(new Usuario(03, 1, "Olvia Silva", "OS", 1));
-        listaUsuarios.add(new Usuario(04, 2, "Alice Fonseca", "AF", 5));
-        listaUsuarios.add(new Usuario(05, 1, "Amanda Rosa", "AR", 4));
-
+        listaUsuarios.add(new Usuario(1, 2, "Claudio Roberto", "CR", 3));
+        listaUsuarios.add(new Usuario(2, 2, "Laura Fonseca", "LF", 2));
+        listaUsuarios.add(new Usuario(3, 1, "Olivia Silva", "OS", 1));
+        listaUsuarios.add(new Usuario(4, 2, "Alice Fonseca", "AF", 5));
+        listaUsuarios.add(new Usuario(5, 1, "Amanda Rosa", "AR", 4));
+        listaUsuarios.add(new Usuario(6, 2, "Ana Paula Correia", "APC", 3));
+        listaUsuarios.add(new Usuario(7, 1, "Roberto Oliveira", "RO", 2));
+        listaUsuarios.add(new Usuario(8, 1, "Giovani Pacheco", "GP", 5));
+        listaUsuarios.add(new Usuario(9, 2, "Mariane Siqueira", "MS", 1));
+        listaUsuarios.add(new Usuario(10, 1, "Marcia Gomes", "MG", 4));
+        listaUsuarios.add(new Usuario(11, 2, "Helena Albuquerque", "HA", 2));
+        listaUsuarios.add(new Usuario(12, 2, "Carlos Cunha", "CC", 1));
+        listaUsuarios.add(new Usuario(13, 1, "Rita Cabedo", "RC", 5));
+        listaUsuarios.add(new Usuario(14, 2, "Valentina Barros", "VB", 3));
+        listaUsuarios.add(new Usuario(15, 2, "Matheus Ramos", "MR", 4));
+        
     }
 
     private static void criarListaDepartamentos(ArrayList<Departamento> listaDepartamentos) {
