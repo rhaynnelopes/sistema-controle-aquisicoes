5a8da9de67d8400a41cb11d4ccc5b70217e06a5d
Iewandowski <Caroline.Rodrigues99@edu.pucrs.br>
2021-10-19 22:33:24 -0300

    modificações - comentários e indentação

--- App.java
+++ App.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-    int menu = 0;
-    ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();        
 
-        Scanner entrada = new Scanner(System.in);
+    int menu = 0;
+    ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
+    Scanner entrada = new Scanner(System.in);
+
     do {
         System.out.println("valor menu = " + menu);
-            System.out.println("SEJA BEM-VINDO!\n");
-            criarEExibirListaUsuarios(listaUsuarios);
-            System.out.println("\n\nSelecione seu usuario pelo id:"); //imprimir lista de usuarios, talvez
-            int idUser = entrada.nextInt();
+        System.out.println("SEJA BEM-VINDO!\n");
+        criarEExibirListaUsuarios(listaUsuarios);
+        System.out.println("\n\nSelecione seu usuario pelo id:");
+
+        int idUser = entrada.nextInt();
+        Usuario userAtual = new Usuario();
         
-            Usuario userAtual = new Usuario();
-            menu++;
-            System.out.println("valor menu = " + menu);
-            for (Usuario user : listaUsuarios)
-            {
-                if(idUser == user.getIdUsuario())
-                userAtual = user;
-            }
+        menu++;
+
+        System.out.println("valor menu = " + menu);
+        for (Usuario user : listaUsuarios)
+        {
+            if(idUser == user.getIdUsuario())
+            userAtual = user;
+        }
         do {            
             switch(userAtual.tipoUsuario)
             {            
@@ -38,16 +41,21 @@ public class App {
                         break;
                         case 2:
                             System.out.println("GERENCIAR REQUISIÇÕES:\n");
-                                //lista de pedidos
-                                //selecionar id do pedido 
-                                //case 1 ou para aprovar ou 2 para rejeitar
-                                break;
+                            //lista de pedidos
+                            //selecionar id do pedido 
+                            //case 1 ou para aprovar ou 2 para rejeitar
+                        break;
                         case 3:
                             System.out.println("ESTATISTICAS GERAIS:\n");
+                            //Número de pedidos total, divididos entre aprovados e reprovados (com percentuais)
+                            //Número de pedidos nos últimos 30 dias e seu valor médio.
+                            //Valor total de cada categoria nos últimos 30 dias
+                            //Detalhes do pedido de aquisição de maior valor ainda aberto.
                         break;
                         case 0:
                             menu--;
                             System.out.println("valor menu = " + menu);
+                            break;                    
                     }
                 break;
                 case 2:
@@ -60,18 +68,18 @@ public class App {
                             //selecionar item por id
                             //selecionar quantidade
                         break;
-
                         case 0:
-                        menu--;
-                        System.out.println("valor menu = " + menu);
+                            menu--;
+                            System.out.println("valor menu = " + menu);
                         break;
                     }                
-                System.out.println("valor menu = " + menu);
                 break;
             }
         }while(menu == 1);
     }while(menu == 0);   
-        entrada.close();
+    
+    
+    entrada.close();
     }
 
     private static void criarEExibirListaUsuarios(ArrayList<Usuario> listaUsuarios) {
