504fd55b0cf84b2b5c4c52376491f7ea057ddb13
Iewandowski <Caroline.Rodrigues99@edu.pucrs.br>
2021-10-21 21:01:13 -0300

    fix pedido
    Co-Authored-By: William Schneider <59919346+pinhak2@users.noreply.github.com>
    Co-Authored-By: Clara D'Ávila <55882141+claracdv@users.noreply.github.com>
    Co-Authored-By: eduardobregalda <91349719+eduardobregalda@users.noreply.github.com>

--- Item.java
+++ Item.java
@@ -1,38 +1,34 @@
-public class Item extends PedidoAquisicao {
-    // numero pedido e valor total já vao ta incluso.
-    //  No pedido aquisição tem valorTotal diferente do total do item?
-    // se for igual, so deixar com o mesmo nome
-    
-    private String descricao;
+public class Item{
+     private String descricao;
     private  double valorUnitario;
     private int quantidade;
-    
-    public Item(int quantidade, double valorUnitario, String descricao){
-        this.quantidade=quantidade;
-        this.valorUnitario=valorUnitario;
-        this.descricao=descricao;
-   } 
-   
-    public String getdDescricao(){
+
+    public Item(String descricao, double valorUnitario, int quantidade){
+        this.descricao = descricao;
+        this.valorUnitario = valorUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao(){
         return descricao;
     }
-    
-    public String setDescricao(String d){
+
+    public void setDescricao(String d){
         this.descricao = d;
     }
-    
+
     public double getValorUnitario(){
         return valorUnitario;
     }
-    
+
     public void setValorUnitario(double vu){
         this.valorUnitario = vu;
     }
-    
+
     public int getQuantidade(){
         return quantidade;
     }
-    
+
     public void setQuantidade(int q){
         this.quantidade = q;
     }
